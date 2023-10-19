36a1e172f0c584ebfdf68970e0abdc77449f1682
searchsakib <searchsakib@gmail.com>
2023-10-20 04:17:27 +0600

    upsert is causing problem

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,9 +36,17 @@ const productCollection = client.db('productDB').collection('product');
 app.get('/', (req, res) => {
   res.send('TechNest server is LOL!');
 });
+
 app.get('/products', async (req, res) => {
   const cursor = productCollection.find();
   const result = await cursor.toArray();
+  res.send(result);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.findOne(query);
   res.send(result);
 });
 
@@ -46,6 +54,25 @@ app.post('/products', async (req, res) => {
   const newProduct = req.body;
   console.log(newProduct);
   const result = await productCollection.insertOne(newProduct);
+  res.send(result);
+});
+
+app.put('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updatedProduct = req.body;
+  const product = {
+    $set: {
+      name: updatedProduct.name,
+      brand: updatedProduct.brand,
+      type: updatedProduct.type,
+      price: updatedProduct.price,
+      rating: updatedProduct.rating,
+      image: updatedProduct.image,
+    },
+  };
+  const result = await productCollection.updateOne(filter, product, options);
   res.send(result);
 });
 
